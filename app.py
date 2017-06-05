42187a43efbe4be2e59c35dd3187a9dd91002e5c
Rahul <rahultimatum@gmail.com>
2017-06-05 08:42:21 -0500

    working on localhost

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import os
+import os,sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:hello@localhost/ascii_db"
 db=SQLAlchemy(app)
-drawings=Drawing.query.all()
 
 class Drawing(db.Model):
     id=db.Column(db.Integer,primary_key=True)
@@ -20,14 +19,23 @@ class Drawing(db.Model):
     def __repr__(self):
         return '<ID %s>' % self.id
 
+db.create_all()
+db.session.commit()
+
 @app.route('/',methods=['GET','POST'])
 def home():
     if request.method=='POST':
-        element= Drawing(request.form['title'], request.form['drawing'])
-        db.session.add(element)
+        title=request.form.get('title')
+        drawing=request.form.get('drawing')
+        deleted=request.form.get('deleted')
+        if title and drawing:
+            element= Drawing(title, drawing)
+            db.session.add(element)
+        if deleted:
+            db.session.delete(Drawing.query.filter_by(id=deleted).first())
         db.session.commit()
 
-    return render_template('doc.html')
+    return render_template('doc.html',drawings=Drawing.query.all())
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
